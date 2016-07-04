47d0066dac8b95275e23064dd6dfd82659994061
Kubota <xyz58344981@yahoo.co.jp>
2016-07-04 09:25:29 +0000

    kadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def show # 追加
    @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
+  end
+  
+  def update
+   if @user.update(user_params)
+     # 保存に成功した場合はトップページへリダイレクト
+     redirect_to root_path , notice: '登録情報を編集しました'
+     else
+     # 保存に失敗した場合は編集画面へ戻す
+    　render 'edit'
+   end
+  end
+  
+  
+  def edit
   end
   
   def create
@@ -18,6 +34,10 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  def set_user
+     @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
